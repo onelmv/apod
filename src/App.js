147ec5e0ba46f89b1205b62334af5b83edfe50a2
Onel Medina <43082323+onelmv@users.noreply.github.com>
2020-06-20 10:13:58 -0400

    change input on searchField

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import Header from './components/Header.js'
 import SearchField from './components/SearchField.js'
@@ -15,15 +15,18 @@ class App extends Component {
     }
   }
   
-
+  /* //* catch every time search field change */
+  onDateFieldChange(event){
+    console.log(event.target.value)
+  }
 
 
   render (){
     return (
       <div className="tc f2 lh-copy georgia bg-light-blue ">
         <Header />
-        <SearchField />
-        <Body date={[this.state.day, this.state.month, this.state.year]} d={this.state}/>
+        <SearchField dateChange={this.onDateFieldChange}/>
+        <Body date={this.state}/> {/*// date={[this.state.day, this.state.month, this.state.year]}//! if you want use arrays insted of an object */} 
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import  React, { Fragment } from 'react';
 const Header = () =>{
     return (
         <Fragment>
-            <div className='pa4 ma0'>
-                <p className=''>Astronomy Picture of the Day</p>
+            <div className='pa4'>
+                <p className=' ma0'>Astronomy Picture of the Day</p>
             </div>
         </Fragment >
     )
