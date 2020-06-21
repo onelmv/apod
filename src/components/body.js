9a04d23a9cf08f3c3f8fbe024f87e47ff491ff8b
Onel Medina <43082323+onelmv@users.noreply.github.com>
2020-06-21 17:31:25 -0400

    fixed : video or image deploy

--- src/components/body.js
+++ src/components/body.js
@@ -2,15 +2,30 @@ import React, { Fragment } from 'react';
 
 
 const Body =(props)=>{
+
+
+    let block = ""
+        if(props.apod.media_type === 'video'){
+            block =  <iframe title='video' className='db w-100  br2 br--top grow tc'  height='450' src={props.apod.url} ></iframe> 
+            console.log(props.apod.url)
+        }else{
+            console.log('image')
+            block = <img src={props.apod.url} className='db w-100 br2 br--top grow' alt={props.apod.title}/>
+        }
+    
+    
     return (
         
         <Fragment>
             <div>
-                {/* {console.log('estoy en body', props.apod.title)} */}
-                {/* {console.log(props.t.componentDidMount(props.date))} */}
                 <div className="br2 ba dark-gray bg-washed-blue b--black-10 mv4 w-90 w-90-m w-90-l mw7 center ">
-                
-                    <img src={props.apod.url} className="db w-100 br2 br--top grow" alt={props.apod.title}/>
+                    
+                    
+                    {/* <img src={props.apod.url} className="db w-100 br2 br--top grow" alt={props.apod.title}/> */}
+                    <div>
+                        {block}
+                    </div>
+
                     <div className="pa2 ph3-ns pb3-ns">
                         <div className="dt w-100 mt1">
                             <div className="tc">
@@ -23,9 +38,6 @@ const Body =(props)=>{
                         <p className="f6 lh-copy pa2 mt2 mid-gray">{props.apod.explanation}</p>
                     </div>
                 </div>
-
-
-
             </div>
             
         </Fragment>
