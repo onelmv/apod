8c594dc9330080467918a3997f42a2a6ba85ee87
Onel Medina <43082323+onelmv@users.noreply.github.com>
2020-06-22 20:48:03 -0400

    Updates

---
+++ precache-manifest.d0ab84c9ec4b30fd3e235b7b9e9adfd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec8548cf05338bffd91af674c4b2f21",
+    "url": "/apod/index.html"
+  },
+  {
+    "revision": "913ab2adaeea88432526",
+    "url": "/apod/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6448fed694cadad92e9c",
+    "url": "/apod/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "913ab2adaeea88432526",
+    "url": "/apod/static/js/2.063f5b2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apod/static/js/2.063f5b2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6448fed694cadad92e9c",
+    "url": "/apod/static/js/main.2d2a9f61.chunk.js"
+  },
+  {
+    "revision": "80abedd48b2b328fb858",
+    "url": "/apod/static/js/runtime-main.ab529de2.js"
+  }
+]);
